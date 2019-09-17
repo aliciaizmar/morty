a2829ded078c1b789e6203ccb8dd4f25eb388c98
younglust <younglust@gmail.com>
2019-09-17 21:18:33 +0200

    Create docs

---
+++ docs/precache-manifest.d6ee0f754eb2f331c0735c15aef58d94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e8e6c99f24d02f36a8027eb43c219d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87fd7f1fad69511f79f3",
+    "url": "./static/css/main.37dfafd3.chunk.css"
+  },
+  {
+    "revision": "fed47513bb8749370750",
+    "url": "./static/js/2.e8c7a45b.chunk.js"
+  },
+  {
+    "revision": "87fd7f1fad69511f79f3",
+    "url": "./static/js/main.da265486.chunk.js"
+  },
+  {
+    "revision": "296b78484dc8f912a7d0",
+    "url": "./static/js/runtime~main.c5effeb1.js"
+  }
+]);
